6bbc664316b1b0b058f230ac71e3df3b3fc93446
999Marlon <137040652+999Marlon@users.noreply.github.com>
2025-03-05 21:16:53 +0100

    Category Stucture

---
+++ backend/ecommerce-shop/src/main/java/com/ecommerce/ecommerce_shop/domain/service/CategoryService.java
@@ -0,0 +1,30 @@
+package com.ecommerce.ecommerce_shop.domain.service;
+
+import java.util.List;
+import com.ecommerce.ecommerce_shop.domain.model.Category;
+import com.ecommerce.ecommerce_shop.domain.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+    }
+}
+
