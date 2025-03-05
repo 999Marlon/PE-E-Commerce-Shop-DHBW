88684ba80a89f1e358b8a4299ec50a74b6c81425
999Marlon <137040652+999Marlon@users.noreply.github.com>
2025-03-05 21:21:35 +0100

    Care Structure

---
+++ backend/ecommerce-shop/src/main/java/com/ecommerce/ecommerce_shop/domain/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_shop.domain.repository;
+
+import com.ecommerce.ecommerce_shop.domain.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByUserId(UUID userId);
+}
