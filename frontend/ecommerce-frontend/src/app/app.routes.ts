580c4d5dbe3cc0323dbf9af0e6af45db6dc422cf
999Marlon <137040652+999Marlon@users.noreply.github.com>
2025-03-05 18:09:49 +0100

    Finished Routes

--- frontend/ecommerce-frontend/src/app/app.routes.ts
+++ frontend/ecommerce-frontend/src/app/app.routes.ts
@@ -2,12 +2,27 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { OrderConfirmationComponent } from './pages/order-confirmation/order-confirmation.component';
 
 export const routes: Routes = [
 
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
+
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
+
+
     { path: 'products', component: ProductsComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: 'products/:id', component: ProductDetailComponent },
+
+    { path: 'cart', component: CartComponent },
+
+    { path: 'order-confirmation', component: OrderConfirmationComponent },
+    { path: 'orders', component: OrdersComponent }, 
+
+
 
 ];
