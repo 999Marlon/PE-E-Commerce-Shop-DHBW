d259289a3d7efbe9da50d42372a81f2cfa4e4670
999Marlon <137040652+999Marlon@users.noreply.github.com>
2025-04-01 13:34:49 +0200

    File eine ebene hoch gelegt

---
+++ backend/ecommerce-shop/0-plugins/src/test/java/com/ecommerce/EcommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EcommerceShopApplication.class)
+class EcommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
